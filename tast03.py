cd8aa885de536f2e759b553265fe5fed2d6af0a6
nii5 <66170787+nii5@users.noreply.github.com>
2020-06-30 15:50:16 +0300

    Add files via upload

--- tast03.py
+++ tast03.py
@@ -1,11 +1,22 @@
-'''
-Реализовать базовый класс Worker (работник),
-в котором определить атрибуты: name, surname, position (должность), income (доход).
-Последний атрибут должен быть защищенным и ссылаться на словарь,
-содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
-Создать класс Position (должность) на базе класса Worker.
- В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и
- дохода с учетом премии (get_total_income).
- Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить
-  значения атрибутов, вызвать методы экземпляров).
-'''
+
+
+class Worker:
+
+    def __init__(self, wage=0, bonus=0):
+        self.name = 'Ivan'
+        self.surname = 'Ivanov'
+        self.position = 'slave'
+        self._income = {"wage": wage, "bonus": bonus}
+
+class Position(Worker):
+
+    def get_full_name(self):
+        print(f'{self.name} {self.surname} {self.position}')
+
+    def get_total_income(self):
+        print(self._income['wage']+self._income['bonus'])
+
+pos = Position(10000, 3000)
+pos.get_full_name()
+pos.get_total_income()
+
